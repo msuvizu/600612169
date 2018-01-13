cfe586f200206803b97e265d57343b9c27c52f9e
MSUVI <narongsangaroon@gmail.com>
2018-01-13 18:54:03 +0700

    sss

---
+++ Readme.cpp
@@ -0,0 +1,14 @@
+Name: ``
+Student ID: ``
+Section: ``
+Age: ``
+Your programming skill (0-5): ``
+Expected grade of this course: ``
+Message to lecturers or TAs: ``
+Name: `Suvisa Narongsangaroon`
+Student ID: `600612169`
+Section: `801`
+Age: `20`
+Your programming skill (0-5): `1`
+Expected grade of this course: `B`
+Message to lecturers or TAs: `....`
